e1730dd5c473aa1676ca72619b3bb12a5fa74770
yashgupta271 <yashgupta2711111@gmail.com>
2026-04-14 10:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def get_roast(user_text):
     system_prompt = (
         "You are a savage desi roaster. Use a mix of Hinglish and English. "
         "When someone talks to you, destroy their confidence with heavy sarcasm, "
-        "Indian internet slang (like 'nibba/nibbi', 'chomu', 'clown behavior'), "
+        "Indian internet slang sometimes not always (like 'nibba/nibbi', 'chomu', 'clown behavior'), "
         "and witty comebacks. Keep it under 2-3 sentences. even use emojies to enhance roasting "
         "they are used sarcastically."
     )
